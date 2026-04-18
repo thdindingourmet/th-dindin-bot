013caa73b36b3bcc6fb7ec6a0944efb22a583cda
thdindingourmet <maryliliane933@gmail.com>
2026-04-18 18:38:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -28,3 +28,21 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+app.post('/asaas', (req, res) => {
+    const data = req.body;
+
+    console.log("Webhook Asaas:", JSON.stringify(data, null, 2));
+
+    const evento = data.event;
+
+    if (evento === "PAYMENT_RECEIVED") {
+        console.log("Pagamento confirmado!");
+
+        // Aqui você pode:
+        // - atualizar pedido
+        // - enviar mensagem no WhatsApp
+    }
+
+    res.sendStatus(200);
+});
